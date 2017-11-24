7c21dd9fc2175b61a88f4e786607552a11473714
Thibault Brocheton <tbrocheton@ecocea.com>
2017-11-24 18:18:21 +0100

    correct update of example / add npmignore to avoid example in npm

--- example/test.js
+++ example/test.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const env = 'dev';
+const env = process.env.ENV || 'dev';
 const configPath = __dirname;
 
+console.log("Loading config");
 var config = require('../').init(configPath, env);
+console.log("Config Loaded");
+console.log("Are we in dev ? "+config.isDev);
